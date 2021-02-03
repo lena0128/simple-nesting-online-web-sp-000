d23bde598929e83ff911ec51a9d10a900c1c0716
Lena Hou <lenahou2013@gmail.com>
2021-02-03 17:27:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
   return programmer_hash
 end
