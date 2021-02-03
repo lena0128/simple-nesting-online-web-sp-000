6a620d942d08b7acb63e90ba2e67f3f7a04d8043
Lena Hou <lenahou2013@gmail.com>
2021-02-03 17:16:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,8 @@ programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash
+programmer_hash[:yukihiro_matsumoto][key]
 end
 
 def changing_alan
